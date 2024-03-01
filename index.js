fc43751e4bd7baefa307f31f27873b130d8897e6
apple <apple@Apples-MacBook-Air.local>
2024-03-01 21:55:03 +0530

    need help

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-let name = "hitesh";
-console.log(Name);
+function isPrime(number) {
+    if (number < 2) {
+        return false;
+    }
+    
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 5) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+console.log(isPrime(5)); 
+console.log(isPrime(10));
